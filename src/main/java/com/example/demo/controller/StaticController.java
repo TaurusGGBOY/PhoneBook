841d99c20b9d12853b41f273ce58f2ef7ab60673
Agaogao <854357635@qq.com>
2019-05-22 20:41:34 +0800

    完成四则运算

--- src/main/java/com/example/demo/controller/StaticController.java
+++ src/main/java/com/example/demo/controller/StaticController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -38,6 +40,71 @@ public class StaticController {
 	public String centerById(@RequestParam("id") String id, @RequestParam("page") String page) {
 
 		return "center";
+	}
+
+	@GetMapping(value = "/cal")
+	public String cal() {
+		return "calculator";
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/cal/answer", method = RequestMethod.POST, headers = "Accept=application/json")
+	public Map<String, Object> answer(@RequestParam String addnum, @RequestParam String addednum,
+			@RequestParam String pattern, @RequestParam String already, @RequestParam String correct,
+			@RequestParam String total_time, @RequestParam String resultans, @RequestParam String time,
+			HttpSession session) {
+
+		/*
+		 * for (Enumeration e = session.getAttributeNames(); e.hasMoreElements();) {
+		 * System.err.println(e.nextElement() + "-----" + session.getAttribute((String)
+		 * e.nextElement())); }
+		 */
+		Map<String, Object> map = new HashMap<>();
+		int[] result = new int[4];
+		try {
+			// 从session中获取随机数
+			boolean isCorrect = false;
+			switch (pattern) {
+			case ("*"):
+				isCorrect = (Integer.parseInt(addnum) * Integer.parseInt(addednum) == Integer.parseInt(resultans));
+				break;
+			case ("+"):
+				isCorrect = (Integer.parseInt(addnum) + Integer.parseInt(addednum) == Integer.parseInt(resultans));
+				break;
+			case ("-"):
+				isCorrect = (Integer.parseInt(addnum) - Integer.parseInt(addednum) == Integer.parseInt(resultans));
+				break;
+			case ("/"):
+				isCorrect = (Integer.parseInt(addnum) / Integer.parseInt(addednum) == Integer.parseInt(resultans));
+				break;
+			}
+			if (isCorrect)
+				result[0] = 1;
+			else
+				result[0] = 0;
+			result[1] = (int) (1 + Math.random() * (1000 - 1 + 1));
+			result[2] = (int) (1 + Math.random() * (1000 - 1 + 1));
+			result[3] = result[1] % 4;
+			// String random = (String) session.getAttribute("RANDOMVALIDATECODEKEY");
+			// System.out.println(username);
+			// System.out.println(password);
+			// System.out.println(sbBuilder.toString());
+
+			// System.out.println(sBuilder.toString());
+			map.put("result0", String.valueOf(result[0]));
+			map.put("result1", String.valueOf(result[1]));
+			map.put("result2", String.valueOf(result[2]));
+			map.put("result3", String.valueOf(result[3]));
+			map.put("already", String.valueOf(Integer.parseInt(already) + 1));
+			map.put("correct", String.valueOf(Integer.parseInt(correct) + result[0]));
+			map.put("total_time", String.valueOf(Integer.parseInt(total_time) + Integer.parseInt(time)));
+
+			return map;
+		} catch (Exception e) {
+			// logger.error("验证码校验失败", e);
+			e.printStackTrace();
+			return new HashMap<>();
+		}
 	}
 
 	/**
